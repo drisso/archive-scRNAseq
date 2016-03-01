a1189fa1eb658b24118f4e05614ef8d628be49f8
Davide Risso <risso.davide@gmail.com>
2016-03-01 12:13:52 -0800

    Initial commit: fluidigm, th2, shalek_dc datasets

---
+++ tmp/create_th2.R
@@ -0,0 +1,37 @@
+genes <- read.table("tmp/gene_list.txt", as.is=TRUE, skip=3)
+cells <- read.table("tmp/cell_list.txt", as.is=TRUE)
+
+counts <- read.table("tmp/tophat2_featureCountsTable.txt", as.is=TRUE)
+fpkm <- read.table("tmp/cuff_fpkmTable.txt", as.is=TRUE)
+
+counts <- counts[genes[,3]!="N/A",]
+fpkm <- fpkm[genes[,3]!="N/A",]
+gene_names <- genes[genes[,3]!="N/A",2]
+
+gene_counts <- apply(counts, 2, function(x) round(tapply(x, gene_names, max, na.rm=TRUE)))
+gene_counts[gene_counts==-Inf] <- NA
+gene_fpkm <- apply(fpkm, 2, function(x) tapply(x, gene_names, max, na.rm=TRUE))
+gene_fpkm[gene_fpkm==-Inf] <- NA
+colnames(gene_counts) <- colnames(gene_fpkm) <- sapply(strsplit(cells[,1], "/"), function(x) x[2])
+
+gene_counts <- na.omit(gene_counts)
+gene_fpkm <- na.omit(gene_fpkm)
+
+geneidx <- intersect(rownames(gene_counts), rownames(gene_fpkm))
+gene_counts <- gene_counts[geneidx,]
+gene_fpkm <- gene_fpkm[geneidx,]
+
+qc <- read.table("tmp/qc_table.txt", as.is=TRUE)
+qc_fields <- read.table("tmp/qc_fields.txt", as.is=TRUE)
+rownames(qc) <- qc_fields[,1]
+colnames(qc) <- colnames(gene_counts)
+
+biotype <- tapply(genes[genes[,3]!="N/A",3], gene_names, unique, na.rm=TRUE)
+
+meta <- read.table("tmp/E-MTAB-2512.sdrf.txt", as.is=TRUE, header=TRUE, sep='\t')
+library(SummarizedExperiment)
+
+th2 <- SummarizedExperiment(assay=list(counts=gene_counts, fpkm=gene_fpkm),
+                           colData=DataFrame(t(qc)), metadata=list(meta))
+
+save(th2, file="data/th2.rda")
