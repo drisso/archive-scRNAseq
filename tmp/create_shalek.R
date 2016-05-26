e9b8aa066d056fc207abafc86cfda1bb003185bb
Davide Risso <risso.davide@gmail.com>
2016-05-25 19:09:06 -0700

    Re-saved Shalek data

--- tmp/create_shalek.R
+++ tmp/create_shalek.R
@@ -1,41 +1,78 @@
-genes <- read.table("tmp/gene_list.txt", as.is=TRUE, skip=3)
-cells <- read.table("tmp/cell_list.txt", as.is=TRUE)
+genes_cuff <- read.table("tmp/shalek/cuff/gene_list.txt", as.is=TRUE, skip=3)
+cells_cuff <- read.table("tmp/shalek/cuff/cell_list.txt", as.is=TRUE)
+genes_rsem <- read.table("tmp/shalek/rsem/gene_list.txt", as.is=TRUE, skip=3)
+cells_rsem <- read.table("tmp/shalek/rsem/cell_list.txt", as.is=TRUE)
 
-counts <- read.table("tmp/tophat2_featureCountsTable.txt", as.is=TRUE)
-fpkm <- read.table("tmp/cuff_fpkmTable.txt", as.is=TRUE)
+tophat_counts <- read.table("tmp/shalek/cuff/tophat2_featureCountsTable.txt", as.is=TRUE)
+cufflinks_fpkm <- read.table("tmp/shalek/cuff/cuff_fpkmTable.txt", as.is=TRUE)
 
-counts <- counts[genes[,3]!="N/A",]
-fpkm <- fpkm[genes[,3]!="N/A",]
-gene_names <- genes[genes[,3]!="N/A",2]
+tophat_counts <- tophat_counts[genes_cuff[,3]=="protein_coding",]
+cufflinks_fpkm <- cufflinks_fpkm[genes_cuff[,3]=="protein_coding",]
+gene_names_cuff <- genes_cuff[genes_cuff[,3]=="protein_coding", 2]
 
-gene_counts <- apply(counts, 2, function(x) round(tapply(x, gene_names, max, na.rm=TRUE)))
-gene_counts[gene_counts==-Inf] <- NA
-gene_fpkm <- apply(fpkm, 2, function(x) tapply(x, gene_names, max, na.rm=TRUE))
-gene_fpkm[gene_fpkm==-Inf] <- NA
-colnames(gene_counts) <- colnames(gene_fpkm) <- sapply(strsplit(cells[,1], "/"), function(x) x[2])
+tophat_counts <- apply(tophat_counts, 2, function(x) round(tapply(x, gene_names_cuff, max, na.rm=TRUE)))
+tophat_counts[tophat_counts==-Inf] <- NA
+cufflinks_fpkm <- apply(cufflinks_fpkm, 2, function(x) tapply(x, gene_names_cuff, max, na.rm=TRUE))
+cufflinks_fpkm[cufflinks_fpkm==-Inf] <- NA
+colnames(tophat_counts) <- colnames(cufflinks_fpkm) <- sapply(strsplit(cells_cuff[,1], "/"), function(x) x[2])
 
-sampleidx <- which(simplify2array(apply(gene_counts, 2, function(x) any(!is.na(x)))))
-gene_counts <- na.omit(gene_counts[,sampleidx])
-gene_fpkm <- na.omit(gene_fpkm[,sampleidx])
+stopifnot(all(rownames(cufflinks_fpkm)==rownames(tophat_counts)))
 
-geneidx <- intersect(rownames(gene_counts), rownames(gene_fpkm))
-gene_counts <- gene_counts[geneidx,]
-gene_fpkm <- gene_fpkm[geneidx,]
+rsem_counts <- read.table("tmp/shalek/rsem/rsem_readCountsTable.txt", as.is=TRUE)
+rsem_tpm <- read.table("tmp/shalek/rsem/rsem_tpmTable.txt", as.is=TRUE)
 
-qc <- read.table("tmp/qc_table.txt", as.is=TRUE)
-qc <- qc[,sampleidx]
+rsem_counts <- rsem_counts[genes_rsem[,3]=="protein_coding",]
+rsem_tpm <- rsem_tpm[genes_rsem[,3]=="protein_coding",]
+gene_names_rsem <- genes_rsem[genes_rsem[,3]=="protein_coding", 2]
+
+rsem_counts <- apply(rsem_counts, 2, function(x) tapply(x, gene_names_rsem, max, na.rm=TRUE))
+rsem_counts[rsem_counts==-Inf] <- NA
+rsem_tpm <- apply(rsem_tpm, 2, function(x) tapply(x, gene_names_rsem, max, na.rm=TRUE))
+rsem_tpm[rsem_tpm==-Inf] <- NA
+colnames(rsem_counts) <- colnames(rsem_tpm) <- sapply(strsplit(cells_rsem[,1], "/"), function(x) x[2])
+
+stopifnot(all(rownames(rsem_tpm)==rownames(rsem_counts)))
+
+geneidx <- intersect(rownames(tophat_counts), rownames(rsem_counts))
+rsem_counts <- rsem_counts[geneidx,]
+rsem_tpm <- rsem_tpm[geneidx,]
+tophat_counts <- tophat_counts[geneidx,]
+cufflinks_fpkm <- cufflinks_fpkm[geneidx,]
+stopifnot(all(colnames(cufflinks_fpkm)==colnames(rsem_tpm)))
+
+qc <- read.table("tmp/shalek/cuff/qc_table.txt", as.is=TRUE)
 qc_fields <- read.table("tmp/qc_fields.txt", as.is=TRUE)
 rownames(qc) <- qc_fields[,1]
-colnames(qc) <- colnames(gene_counts)
+colnames(qc) <- colnames(cufflinks_fpkm)
+stopifnot(all(colnames(qc)==colnames(tophat_counts)))
 
-biotype <- tapply(genes[genes[,3]!="N/A",3], gene_names, unique, na.rm=TRUE)
+remove_samples <- which(apply(tophat_counts, 2, function(x) all(is.na(x))))
 
-batch <- read.table("tmp/batch_final.tab", as.is=TRUE)
-meta1 <- read.csv("tmp/SRP027537_info.csv", as.is=TRUE, header=TRUE)
+geo <- read.table("tmp/shalek/sample.tsv", sep='\t', header=TRUE, stringsAsFactors = FALSE)
+sra <- read.table("tmp/shalek/SraRunTable.txt", sep='\t', header=TRUE, stringsAsFactors = FALSE)
+batch <- read.table("tmp/shalek/batch_final.tab", as.is=TRUE)
+
+md <- merge(geo, sra, by.x = "SRA.Accession", by.y="Experiment_s")
+
+id <- tapply(md$Title, md$SRA.Accession, unique)
+stim <- tapply(md$stimulation_s, md$SRA.Accession, unique)
+bb <- batch[,2]
+names(bb) <- batch[,1]
+stopifnot(all(names(bb)==names(id)))
+
+ccd <- data.frame(sample_id=id, stimulation=stim, batch=bb)
+ccd <- ccd[colnames(qc),]
+
+stopifnot(all(rownames(ccd) == colnames(qc)))
 
 library(SummarizedExperiment)
 
-shalek_dc <- SummarizedExperiment(assay=list(counts=gene_counts, fpkm=gene_fpkm),
-                           colData=DataFrame(t(qc)), metadata=list(metadata=meta1, batch=batch))
+shalek_dc <- SummarizedExperiment(assay=list(tophat_counts = tophat_counts[,-remove_samples],
+                                             cufflinks_fpkm = cufflinks_fpkm[,-remove_samples],
+                                             rsem_counts = rsem_counts[,-remove_samples],
+                                             rsem_tpm = rsem_tpm[,-remove_samples]),
+                                  colData=DataFrame(cbind(t(qc), ccd)[-remove_samples,]),
+                                  metadata=list(GEO_SRA=md,
+                                                which_qc=rownames(qc)))
 
-save(shalek_dc, file="data/shalek_dc.rda")
+save(shalek_dc, file="data/shalek_dc.rda", compress="xz")
